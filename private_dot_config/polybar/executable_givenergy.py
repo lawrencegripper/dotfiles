742bd5cd22b8a7b26266da4b8314940fef590d90
lawrencegripper <info@grippers.co.uk>
2023-02-16 12:37:38 +0000

    Show status surplus or not

--- private_dot_config/polybar/executable_givenergy.py
+++ private_dot_config/polybar/executable_givenergy.py
@@ -9,16 +9,28 @@ try:
     p = Plant(number_batteries=1)
     client.refresh_plant(p, full_refresh=True)
 
+    def format_color(color: str, text: str):
+        return "%{F" + color + "}" + text + "%{F-}"
+
     def format_wattage(input: int):
         if input > 1000:
             return f"{input/1000}Kw"
         return f"{input}w"
 
-    output = "%{F#ffff1a}  %{F-}"
-    output += "  " + format_wattage(p.inverter.p_load_demand)
-    output += " 󰶛 " + format_wattage(p.inverter.p_pv1 + p.inverter.p_pv2)
-    output += "  " + str(p.inverter.battery_percent) + "%"
-    output += "  " + format_wattage(p.inverter.p_grid_out)
+    def format_icon(input: str):
+        return format_color("#F0C674", f" {input} ")
+
+    surplus = p.inverter.p_load_demand < (p.inverter.p_pv1 + p.inverter.p_pv2)
+    if surplus:
+        lightening_color = "#2d862d"
+    else:
+        lightening_color = "#b30000"
+
+    output = format_color(lightening_color, "")
+    output += format_icon("") + format_wattage(p.inverter.p_load_demand)
+    output += format_icon("󰶛") + format_wattage(p.inverter.p_pv1 + p.inverter.p_pv2)
+    output += format_icon("") + str(p.inverter.battery_percent) + "%"
+    output += format_icon("") + format_wattage(p.inverter.p_grid_out)
 
     print(output)
 except Exception as e:

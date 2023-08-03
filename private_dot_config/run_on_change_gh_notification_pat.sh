28ee408c05b3a0b4d44a64b025565dd95108bdfa
lawrencegripper <info@grippers.co.uk>
2023-08-03 11:02:17 +0100

    Fix dotfiles in codespace

--- private_dot_config/run_on_change_gh_notification_pat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f ~/.gh-notifications
-if type "op" > /dev/null; then
-    op read "op://Personal/github.com/PATS/gh-notifications" > ~/.config/gh-notifications
-    chmod 600 ~/.config/gh-notifications
-else
-    echo "Failed not available"
-fi

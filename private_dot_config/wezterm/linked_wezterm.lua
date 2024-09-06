49b9d457a5cfa231fa170ce7fb18d6783472405e
lawrencegripper <info@grippers.co.uk>
2024-09-06 09:22:31 +0100

    Tweaks

--- private_dot_config/wezterm/linked_wezterm.lua
+++ private_dot_config/wezterm/linked_wezterm.lua
@@ -11,5 +11,20 @@ config.color_scheme = 'Material (Gogh)'
 config.font_size = 18
 config.font = wezterm.font('JetBrains Mono Nerd Font', { italic = false })
 
+config.scrollback_lines = 90000
+
+config.ssh_domains = {
+    {
+      -- This name identifies the domain
+      name = 'libvirt',
+      -- The hostname or address to connect to. Will be used to match settings
+      -- from your ssh config file
+      remote_address = 'libvirt',
+      -- The username to use on the remote host
+      username = 'lawrencegripper',
+    },
+  }
+  
+
 -- and finally, return the configuration to wezterm
 return config

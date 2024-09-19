d75bbbd805153dbe02d54a7511f2158710b92e82
lawrencegripper <info@grippers.co.uk>
2024-09-19 10:25:38 +0100

    Tweaks

--- private_dot_config/wezterm/linked_wezterm.lua
+++ private_dot_config/wezterm/linked_wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- config.color_scheme = 'Monokai Pro (Gogh)'
-config.color_scheme = 'Material (Gogh)'
+config.color_scheme = 'MaterialDesignColors'
 config.font_size = 18
 config.font = wezterm.font('JetBrains Mono Nerd Font', { italic = false })
 

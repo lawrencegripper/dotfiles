15242b4ab326884c91bb731eb32b60a24bddb435
lawrencegripper <info@grippers.co.uk>
2025-01-10 12:29:31 +0000

    Fix

--- private_dot_config/regolith3/sway/executable_sway_dropdown_terminal.sh
+++ private_dot_config/regolith3/sway/executable_sway_dropdown_terminal.sh
@@ -7,7 +7,6 @@ then
     # If multi-monitor configuration: resize on each monitor
     swaymsg "[ pid=$TERM_PID ] resize set 100ppt , move position 0 0"
 else
-	echo 
     echo "$$" > "$TERM_PIDFILE"
     swaymsg "for_window [ pid=$$ ] 'floating enable ; resize set 100ppt 50ppt ; move position 0 0 ; move to scratchpad ; scratchpad show'"
     exec wezterm connect unix-dropdown

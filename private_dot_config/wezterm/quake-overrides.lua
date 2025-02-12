46493a9772219c38aebae0710caf339d3bd38803
lawrencegripper <i+gitpersonal@grippers.co.uk>
2025-02-12 09:49:03 +0000

    tweaks for dropdown shells

--- private_dot_config/wezterm/quake-overrides.lua
+++ private_dot_config/wezterm/quake-overrides.lua
@@ -2,7 +2,7 @@ local module = {}
 
 function module.apply(config)
     -- override the transparency
-    config.window_background_opacity = 0.5
+    config.window_background_opacity = 0.8
     config.text_background_opacity = 1
     config.color_scheme = 'Monokai Pro (Gogh)'
 end

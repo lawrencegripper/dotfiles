f5155e87766676d71ee7df7a1655eb10833ca220
lawrencegripper <i+gitpersonal@grippers.co.uk>
2025-09-09 11:24:14 +0100

    tweaks

--- private_dot_config/wezterm/codespace_template.lua
+++ private_dot_config/wezterm/codespace_template.lua
@@ -5,7 +5,15 @@ config.automatically_reload_config = true
 config.unix_domains = {
     {
         name='CS_NAME',
-        proxy_command = { '/home/linuxbrew/.linuxbrew/bin/gh', 'cs', 'ssh', '-c', 'CS_NAME', '--', '-R', '3123:100.110.255.124:80', '-R', '8123:languagetool-api.unicorn-tailor.ts.net:8123', 'wezterm', 'cli', 'proxy' }
+        proxy_command = {
+            '/home/linuxbrew/.linuxbrew/bin/gh', 'cs', 'ssh', '-c', 'CS_NAME',
+            '--',
+            -- Port forwarding
+            '-R', '3123:atuin-api.unicorn-tailor.ts.net:80',
+            '-R', '8123:languagetool-api.unicorn-tailor.ts.net:8123',
+            -- Wezterm proxy
+            'wezterm', 'cli', 'proxy'
+        }
     }
 }
 
